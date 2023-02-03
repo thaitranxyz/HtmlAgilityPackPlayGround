dad282985ca74c06db5cf38baa4f261641e14320
thaitran <43898236+thaitranxyz@users.noreply.github.com>
2023-02-03 20:11:12 +1100

    Add project files.

---
+++ practicenumber10000/Program.cs
@@ -0,0 +1,61 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlAgilityPackPlayGround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://en.wikipedia.org/wiki/List_of_programmers";
+            var response = CallUrl(url).Result;
+
+            var linkList = ParseHtml(response); 
+            WriteToCsv(linkList);
+
+            Console.WriteLine(linkList);
+        }
+
+        public static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(fullUrl); 
+            return response; 
+        }
+
+        private static void WriteToCsv(List<string> links)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var link in links)
+            {
+                sb.AppendLine(link);
+            }
+
+            System.IO.File.WriteAllText("links.csv", sb.ToString()); 
+        }
+
+        private static List<string> ParseHtml(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var programmerLinks = htmlDoc.DocumentNode.Descendants("li")
+                .Where(node => !node.GetAttributeValue("class", "")
+                .Contains("tocsection")).ToList(); 
+
+            List<string> wikiLink = new List<string>(); 
+
+            foreach (var link in programmerLinks)
+            {
+                if (link.FirstChild.Attributes.Count > 0) wikiLink.Add("https://en.wikipedia.org/" + link.FirstChild.Attributes[0].Value);
+            }
+
+            return wikiLink; 
+        }
+    }
+}
